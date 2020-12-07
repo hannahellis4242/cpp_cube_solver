6fee47551a004029eaf57a1f2f842d03f59265d9
hannahellis4242 <hannahellis4242@gmail.com>
2020-12-07 17:20:57 +0000

    Adding an interface for solution space and moving things around.

--- SolutionSpace.c
+++ SolutionSpace.c
@@ -6,28 +6,26 @@
 #include "Configuration.h"
 #include "Instruction.h"
 
-namespace cube
-{
-  typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS,
-                                Configuration, Instruction>
-      Graph;
-  typedef boost::graph_traits<Graph>::vertex_descriptor vertex_descriptor;
-  typedef boost::graph_traits<Graph>::edge_descriptor edge_descriptor;
+namespace cube {
+using Graph = boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS,
+                                    Configuration, Instruction>;
+using vertex = boost::graph_traits<Graph>::vertex_descriptor;
+using edge = boost::graph_traits<Graph>::edge_descriptor;
 
-  class GraphWriter {
-  private:
-    const Graph &graph_;
+class GraphWriter {
+private:
+  const Graph &graph_;
 
-  public:
-    GraphWriter(const Graph &graph) : graph_(graph) {}
-    void operator()(std::ostream &) const {}
-    void operator()(std::ostream &os, const vertex_descriptor v) const {
-      const Configuration &config = graph_[v];
-      os << "[label=\"" << show(config) << "\"]";
-    }
-    void operator()(std::ostream &os, const edge_descriptor e) const {
-      const Instruction &x = graph_[e];
+public:
+  GraphWriter(const Graph &graph) : graph_(graph) {}
+  void operator()(std::ostream &) const {}
+  void operator()(std::ostream &os, const vertex_descriptor v) const {
+    const Configuration &config = graph_[v];
+    os << "[label=\"" << show(config) << "\"]";
+  }
+  void operator()(std::ostream &os, const edge_descriptor e) const {
+    const Instruction &x = graph_[e];
       os << "[label=\"<< show(x)<<"\]";
-    }
-  };
-}
+  }
+};
+} // namespace cube
--- SolutionSpace.h
+++ SolutionSpace.h
@@ -1,6 +1,20 @@
 #ifndef SOLUTIONSPACE_H
 #define SOLUTIONSPACE_H
 
+#include "Configuration.h"
+#include "Instruction.h"
 
+#include <vector>
+namespace cube {
+using InstructionAndWeight = std::pair<Instruction, unsigned int>;
+class SolutionSpace {
+public:
+  std::vector<InstructionAndWeight> next(const Configuration &,
+                                         const Instruction) const;
+  const Configuration &visit(const Configuration &, const Instruction);
+
+  std::string show() const;
+};
+} // namespace cube
 
 #endif
